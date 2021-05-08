e07955d6e249e41b332087a705f242fe3ca79555
glbetgy <tuizaiye680963@126.com>
2021-05-08 15:18:40 +0800

    基础项目框架

---
+++ src/com/wy/dao/UserDAO.java
@@ -0,0 +1,23 @@
+package com.wy.dao;
+
+import java.util.List;
+
+import com.wy.entity.User;
+
+public interface UserDAO {
+
+	    //添加用户
+		public void add(User user);
+		
+		//删除用户
+		public void delete(User user);
+		
+		//更新用户
+		public void update(User user);
+		
+		//查询所有用户
+		public List queryAll();
+		
+		//按id查询用户
+		public User queryByID(String username);
+}
